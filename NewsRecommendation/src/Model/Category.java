b562a56344c37c9910b581e13f259091e59ea4ce
vihanga induwara <20indu03@gmail.com>
2024-12-05 03:34:33 +0530

    update Category class

--- NewsRecommendation/src/Model/Category.java
+++ NewsRecommendation/src/Model/Category.java
@@ -1,4 +1,29 @@
 package Model;
 
 public class Category {
+    private String name;
+    private String description;
+
+    // Constructor to initialize category
+    public Category(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    // Getters and Setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
